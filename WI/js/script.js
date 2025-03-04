318f38567cb71b5ecf525169dc597d20554046c5
David van Hoek <111139186+TurtlePrograms@users.noreply.github.com>
2025-03-04 11:24:57 +0100

    chore: improve dropdown functionality and update footer logo styles

--- WI/js/script.js
+++ WI/js/script.js
@@ -1,11 +1,15 @@
+const MobileDropDown = document.getElementById("mobileDropDown");
+const MobileDropDownButton = document.getElementById("mobileDropDownButton");
+
 function toggleDropdown() {
-    document.getElementById("mobileDropDown").classList.toggle("show");
-    document.getElementById("mobileDropDownButton").classList.toggle("show");
+    MobileDropDown.classList.toggle("show");
+    MobileDropDownButton.classList.toggle("show");
 }
+
 document.onclick = function (event) {
     // Close the dropdown if the user clicks outside of it
-    if (!event.target.matches("#mobileDropDownButton") && !event.target.matches("#mobileDropwDownButtonText") && !event.target.matches(".mobileDropDownContent")) {
-        document.getElementById("mobileDropDown").classList.remove("show");
-        document.getElementById("mobileDropDownButton").classList.remove("show");
+    if (!MobileDropDownButton.contains(event.target) && !event.target.matches("#mobileDropwDownButtonText") && !MobileDropDown.contains(event.target)) {
+        MobileDropDown.classList.remove("show");
+        MobileDropDownButton.classList.remove("show");
     }
 }
