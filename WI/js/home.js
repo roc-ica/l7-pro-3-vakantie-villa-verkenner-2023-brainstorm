c9be6a0b20a4135a289275ee5de0b68c1f433a05
David <111139186+TurtlePrograms@users.noreply.github.com>
2025-03-12 13:14:47 +0100

    fix: homepage style

--- WI/js/home.js
+++ WI/js/home.js
@@ -1,6 +1,15 @@
 
 async function getVillas() {
-    const villas = await VillaRequests.getVillasByIDs([2, 5])
+    const ids = [2, 5];
+    for (let i = 0; i < ids.length; i++) {
+        document.getElementsByClassName('VillaContainer')[0].innerHTML += `
+            <div class="villaCard">
+                <p style="margin-left:5px;">Loading...</p>
+            </div>
+        `;
+    }
+    const villas = await VillaRequests.getVillasByIDs(ids)
+    document.getElementsByClassName('VillaContainer')[0].innerHTML = '';
     villas.forEach(villa => {
         let smallVilla = new SmallVilla(villa);
         document.getElementsByClassName('VillaContainer')[0].innerHTML += smallVilla.html;
