44a6aecff9271212b5ec43490c1791951260fe0f
David <111139186+TurtlePrograms@users.noreply.github.com>
2025-03-19 10:53:10 +0100

    chore: update database models

---
+++ API/VillaVerkenerAPI/Models/DB/PropertyTag.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VillaVerkenerAPI.Models.DB;
+
+public partial class PropertyTag
+{
+    public int PropertyTagId { get; set; }
+
+    public string PropertyTag1 { get; set; } = null!;
+}
---
+++ API/VillaVerkenerAPI/Models/DB/VillaPropertyTag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VillaVerkenerAPI.Models.DB;
+
+public partial class VillaPropertyTag
+{
+    public int VillaId { get; set; }
+
+    public int PropertyTagId { get; set; }
+
+    public virtual PropertyTag PropertyTag { get; set; } = null!;
+
+    public virtual Villa Villa { get; set; } = null!;
+}
